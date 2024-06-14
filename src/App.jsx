4829b07ddaaec76aa2a50f3c775a4d42a7a448c2
Xcodes <ntmark2004@gmail.com>
2024-06-14 01:29:29 +0100

    hero page done 1/2

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/pages/Home";
 import AppLayout from "./components/ui/AppLayout";
 import ErrorPage from "./components/pages/ErrorPage";
-import { useState } from "react";
-import viteLogo from "/vite.svg";
-import "./App.css";
 
 function App() {
 	return (
@@ -14,18 +11,8 @@ function App() {
 					<Route index element={<Home />} />
 					<Route path='home' element={<Home />} />
 				</Route>
-				<Route path='*' element={<ErrorPage />} />
-			</Routes>
-		</BrowserRouter>
-	);
-	return (
-		<BrowserRouter>
-			<Routes>
-				<Route path='/' element={<AppLayout />}>
-					<Route index element={<Home />} />
-					<Route path='home' element={<Home />} />
-				</Route>
 				<Route path='properties' element={<PropertyPage />} />
+				<Route path='*' element={<ErrorPage />} />
 			</Routes>
 		</BrowserRouter>
 	);
