645508f01a38244628661e472407c825cbc1e454
Uba chukwuka promise <promiseuba67@gmail.com>
2024-06-14 14:56:13 +0100

    swichted to nextjs

--- src/store/user-store.js
+++
@@ -1,8 +0,0 @@
-import { create } from "zustand";
-
-export const useUserStore = create((set) => ({
-  user: null,
-  setUser: () => set((state) => ({ bears: state.bears + 1 })),
-  removeUser: () => set({ bears: 0 }),
-  updateUser: (newBears) => set({ bears: newBears }),
-}));
